7b78d21cea58fab7066a063cdd7d5642e3882ea7
zaswed76 <zaswed76@gmail.com>
2023-04-11 21:33:44 +0300

    dialoge +++

--- Game24/aichat.py
+++ Game24/aichat.py
@@ -1,22 +1,22 @@
 
 
-api = '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'
-
-
-import openai
-
-openai.api_key = api
-
-def aichat(key):
-            response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=[
-                        {"role": "system", "content": "You are a bot"},
-                        {"role": "user", "content": key},])
-            result = ""
-            for choice in response.choices:
-                result += choice.message.content
-            return result
-if __name__ == "__main__":
-
-    print(aichat("скажи щось смішне одним реченням"))
+# api = ''
+#
+#
+# import openai
+#
+# openai.api_key = api
+#
+# def aichat(key):
+#             response = openai.ChatCompletion.create(
+#                 model="gpt-3.5-turbo",
+#                 messages=[
+#                         {"role": "system", "content": "You are a bot"},
+#                         {"role": "user", "content": key},])
+#             result = ""
+#             for choice in response.choices:
+#                 result += choice.message.content
+#             return result
+# if __name__ == "__main__":
+#
+#     print(aichat("скажи щось смішне одним реченням"))
--- Game24/sprites/mooving_platform.py
+++ Game24/sprites/mooving_platform.py
@@ -1,8 +1,10 @@
 
 def update_mooving_platform(layer, physics_engine, delta_time):
+
     for moving_sprite in layer:
         # print(moving_sprite.properties['active'], moving_sprite.properties['name'], 4444444)
         if moving_sprite.properties['active']:
+            print(0)
             if moving_sprite.boundary_right and \
                     moving_sprite.right > moving_sprite.boundary_right:
                 moving_sprite.change_x *= -1
@@ -13,11 +15,13 @@ def update_mooving_platform(layer, physics_engine, delta_time):
             if moving_sprite.boundary_top and \
                     moving_sprite.change_y > 0 and \
                     moving_sprite.top > moving_sprite.boundary_top:
+                print(1111)
                 moving_sprite.change_y *= -1
             elif moving_sprite.boundary_bottom and \
                     moving_sprite.change_y < 0 and \
                     moving_sprite.bottom < moving_sprite.boundary_bottom:
                 moving_sprite.change_y *= -1
+                print(2222)
             velocity = (moving_sprite.change_x * 1 / delta_time, moving_sprite.change_y * 1 / delta_time)
             physics_engine.set_velocity(moving_sprite, velocity)
         else:
